2ce05ce8345618d2cc249345b69a962b5faf4816
kabumko <kabumko@abv.bg>
2015-01-13 11:57:38 +0200

    .init

---
+++ 04.FirstNameLastName/FirstNameLastName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+/*
+    Create console application that prints your first and last name, each at a separate line.   Check.    
+ */
+class FirstNameLastName
+{
+    static void Main()
+    {
+        Console.WriteLine("First Name:       Teodor");
+        Console.WriteLine("Last Name:        Arhondaki");
+    }
+}
---
+++ 05.SquareRoot/SquareRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+/*
+    Create a console application that calculates and prints the square root of the number 12345.                                                Check
+    Find in Internet “how to calculate square root in C#.   http://www.sanfoundry.com/c-program-to-find-square-root-of-a-given-number/          Check
+ */
+
+class SquareRoot
+{
+    static void Main()
+    {
+        Console.WriteLine(Math.Sqrt(12345));
+    }
+}
+
